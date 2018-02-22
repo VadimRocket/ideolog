4bc95515fd6ed54d506d60d7c3ab03bc5164a6e8
Vadim <fragariastar@gmail.com>
2018-02-22 19:39:25 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@
             <div>
                 <div>
 					<h3  class="tlt p_404"><span class="tlt">Not Found (#404)   </span> </h3>
-                     <p class="tlt p_404">Страница не найдена.Вернитесь пожалуйста обратно.</p>
+                     <p class="tlt p_404">Страница не найдена.Вернитесь пожалуйста обратно.<a href="javascript: history.go(-1)">
+                Вернуться обратно </a></p>
                 </div>
             </div>
      
